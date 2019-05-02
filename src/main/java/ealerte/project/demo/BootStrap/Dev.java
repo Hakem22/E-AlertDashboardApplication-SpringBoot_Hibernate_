b6f6dfd3abcd91453f5d501da97d79649cb3745d
Imene Hakem <vidya-imen@hotmail.fr>
2019-05-02 09:11:18 +0100

    Update 02/05

--- src/main/java/ealerte/project/demo/BootStrap/Dev.java
+++ src/main/java/ealerte/project/demo/BootStrap/Dev.java
@@ -59,7 +59,7 @@ public class Dev implements ApplicationListener<ContextRefreshedEvent> {
         Agent agent= new Agent( "Nadir","Otman bey", "0487619200", "Nadir31","1123");
         Address address=new Address("Aissat idir", "23","22002","Sidi Yacine");
         LocalisationU localisationU=new LocalisationU(45.98, 67.976, 7.99);
-        InterventionUnit interventionUnit= new InterventionUnit(InterventionType.GENDARMERIE,address, localisationU);
+        InterventionUnit interventionUnit= new InterventionUnit(InterventionType.GENDARMERIE,localisationU, address);
         LocalisationA localisationA=new LocalisationA( 23.999,2.98,3.09);
         LocalisationA localisationA1= new LocalisationA( 50.999,0.98,9.09);
       //  Media media=new Media("lien");
@@ -70,9 +70,12 @@ public class Dev implements ApplicationListener<ContextRefreshedEvent> {
         Citizen citizen= new Citizen("0795813471");
         Sensor sensor=new Sensor("H123", SensorState.ACTIVE, SensorType.WATER_LEVEL,3.4,4.7);
         AlertS alertS= new AlertS(LocalDate.now(), LocalTime.now(), 2.1,localisationA);
-        sensor.addAlertS(alertS);
+        alertS.setSensor(sensor);
+        alertC.setCitizen(citizen);
+        //sensor.addAlertS(alertS);
         Admin admin=new Admin("Imen", "Hakem","0487619200","Imen22","Imen1995@");
 
+        alertS.setActeur(admin);
         adminRepository.save(admin);
         sensorRepository.save(sensor);
         citizenRepository.save(citizen);
