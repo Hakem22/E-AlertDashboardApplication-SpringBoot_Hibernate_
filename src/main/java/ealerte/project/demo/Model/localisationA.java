11cd4c6b9c18586a12f92477044fa1a36906e49d
Imene Hakem <vidya-imen@hotmail.fr>
2019-05-02 10:20:07 +0100

    Update 02/05

--- src/main/java/ealerte/project/demo/Model/localisationA.java
+++ src/main/java/ealerte/project/demo/Model/localisationA.java
@@ -7,24 +7,25 @@ import javax.validation.constraints.NotNull;
 @Entity
 @Table(name="localisationA")
 public class LocalisationA extends Localisation implements Comparable<Localisation> {
-
+/*
     @OneToOne(mappedBy = "localisationA")
     private Alert alert;
-
+*/
     public LocalisationA(){ super ();}
 
     public LocalisationA(@NotNull Double altitude, @NotNull Double longitude, @NotNull Double rayon) {
         super( altitude, longitude, rayon);
     }
-
+/*
     public void setAlert(Alert alert) {
         this.alert = alert;
     }
-
+*/
+/*
     public Alert getAlert() {
         return alert;
     }
-
+*/
     public int compareTo(Localisation localisation){
         if(this.getAltitude()+this.getRayon()>localisation.getAltitude() && this.getLongitude()+this.getRayon()>localisation.getLongitude()) return 1;
         return 0;
